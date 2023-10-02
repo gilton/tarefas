e7eb7ed5a1fb5f20a77dc230e3e32440fa69b6f0
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2023-10-02 18:10:18 -0300

    fix: type custo bigInt to Float

--- src/main/java/com/desafio/tarefas/entity/Tarefa.java
+++ src/main/java/com/desafio/tarefas/entity/Tarefa.java
@@ -1,33 +1,50 @@
 package com.desafio.tarefas.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.math.BigInteger;
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.UUID;
 
-@Table(schema = "public", name = "Tarefas")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Table(name = "Tarefas")
 @Entity
 @Data
 @Builder
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
-public class Tarefa {
+public class Tarefa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int idTarefa;
+    private UUID idTarefa;
+
+    @Column(name = "nom_tarefa", nullable = false, unique = true, length = 120)
     private String nomTarefa;
-    private BigInteger custo;
+
+    @Column(nullable = false)
+    private Float custo;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    @Column(name = "data_limite")
     private LocalDate dataLimite;
+
+    @Column(name = "ordem")
     private int ordem;
 }
