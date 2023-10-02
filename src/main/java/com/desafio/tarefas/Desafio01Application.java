e61beae6dab5e22c5c55cc6e729df0616a4b9123
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2023-10-02 18:04:35 -0300

    feat: config to postgresql database

--- src/main/java/com/desafio/tarefas/Desafio01Application.java
+++ src/main/java/com/desafio/tarefas/Desafio01Application.java
@@ -2,12 +2,20 @@ package com.desafio.tarefas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Desafio01Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Desafio01Application.class, args);
     }
 
+
+    @GetMapping("/")
+    public String index(){
+        return "Ola Mundo.";
+    }
 }
